453399e65caebe36e7adc068ae8c5b2b83d349d8
Hudaa516 <mohammedhuda080@gmail.com>
2024-09-26 03:54:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let wok = [{
+    name: "Wok1",
+    price: 150,
+    img: "./wok1.jpg",
+},
+{
+    name: "Wok2",
+    price: 120,
+    img: "./wok2.jpg",
+},
+{
+    name: "Wok3",
+    price: 70,
+    img: "./wok3.jpg",
+},
+{
+    name: "Wok4",
+    price: 60,
+    img: "./wok1.jpg",
+},
+{
+    name: "Wok5",
+    price: 100,
+    img: "./wok2.jpg",
+},
+{
+    name: "Wok6",
+    price: 60,
+    img: "./wok3.jpg",
+}];
---
+++ regexPatterns.js
@@ -0,0 +1,5 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex =
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>?,./])(?=.{6,20}$).+$/;
+const ftnameRegex = /^(.{2,15})$/;
+const ltnameRegex = /^(.{2,15})$/;
